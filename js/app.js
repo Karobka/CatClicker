80538edcd37ea186a5760a6494c0d3eaab0710c9
Tim Wright <mithgirw@gmail.com>
2016-07-23 16:47:48 -0600

    two cats

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 "use strict";
 var count = 0;
 $(document).ready(function() {
+    $(".meow1").text("Fluffy");
+    $(".meow2").text("FooFoo");
     $(".cat-pic").on("click", function(event) {
         count++;
         $(".count-num").text(count);
